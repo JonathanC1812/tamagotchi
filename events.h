36dfaa13ebeb2d779c9a4b2860f520211e99a10a
Jovan Mikael Johansen <101058850+JvnJhnsn@users.noreply.github.com>
2026-05-02 06:38:16 +0800

    Update events.h

--- events.h
+++ events.h
@@ -1,13 +1,10 @@
 #ifndef events.h
 #define events.h
-
 #include "common.h"
 #include <string>
-
 std::string rollEvent(GameState& gs);
 void applyNegativeEvent(Pet& pet, DifficultyLevel diff, std::string& msg);
 void applyPositiveEvent(Pet& pet, std::string& msg);
 void applyNeutralEvent(Pet& pet, std::string& msg);
 bool applyStageEvent(Pet& pet, std::string& msg);
-
 #endif
