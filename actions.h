de9e47833864595bc529e468e2ebb4e57c2c2314
Darrel Pras <darrelnathan054@gmail.com>
2026-05-02 15:46:02 +0800

    Add actionWork and actionBath function declarations

--- actions.h
+++ actions.h
@@ -5,6 +5,8 @@ std::string actionFeed(GameState& gs, int idx);
 std::string actionPlay(GameState& gs, int idx);
 std::string actionSleep(GameState& gs);
 std::string actionHeal(GameState& gs, int idx);
+std::string actionWork(GameState& gs, int left, int right, char op, int answer);
+std::string actionBath(GameState& gs);
 std::string actionStatus(const GameState& gs);
 bool canAct(const Pet& pet);
 #endif
